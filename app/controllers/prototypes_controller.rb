cd7121dcf6b98340f86ab3c25ed3e7af4e043ede
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2022-03-27 19:30:16 +0900

    修正２

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create,]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :move_to_index, except: [:index, :show, :new, :create,:edit,:update,:destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
 
   def index
@@ -22,19 +22,17 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
     prototype = Prototype.find(params[:id])
     if prototype.update(prototype_params)
-      redirect_to prototype_path
+      redirect_to prototype_path(@prototype)
     else
       render :edit
     end
@@ -42,8 +40,11 @@ class PrototypesController < ApplicationController
 
   def destroy
     prototype = Prototype.find(params[:id])
-    prototype.destroy
-    redirect_to root_path
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
 
@@ -53,12 +54,13 @@ class PrototypesController < ApplicationController
      params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
    end
 
-   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+   def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
   def contributor_confirmation
     redirect_to root_path unless current_user == @prototype.user
   end
+
 
 end
